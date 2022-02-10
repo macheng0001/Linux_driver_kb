d3fabe1278b566f7627705905c0cfa143d2aee35
machengyuan <machengyuan@coinv.com>
2022-02-09 22:18:55 -0800

    添加字符设备驱动doc和driver

---
+++ 1.字符设备/driver/c-drv.c
@@ -0,0 +1,141 @@
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/ide.h>
+#include <linux/types.h>
+#include <linux/errno.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/module.h>
+
+/* 设备节点名称 */
+#define DEVICE_NAME       "char_dev"
+/* 设备号个数 */
+#define DEVID_COUNT       1
+/* 驱动个数 */
+#define DRIVE_COUNT       1
+/* 主设备号 */
+#define DEV_MAJOR
+/* 次设备号 */
+#define DEV_MINOR             0
+
+struct char_dev{
+	dev_t            devid;      //设备号
+	struct cdev      cdev;       //字符设备
+	struct class     *class;     //类
+	struct device    *device;    //设备节点
+};
+
+static struct char_dev char_dev = {
+	.cdev = {
+		.owner = THIS_MODULE,
+	},
+};
+
+static int char_dev_open(struct inode *inode, struct file *file)
+{
+		struct char_dev *dev = container_of(inode->i_cdev, struct char_dev, cdev);
+		struct char_dev *drvdata = dev_get_drvdata(dev->device);
+
+		printk("device name:%s\n", dev_name(dev->device));
+		printk("driver data device name:%s\n", dev_name(drvdata->device));
+    printk("char dev open\n");
+
+		file->private_data = dev;
+    return 0;
+}
+
+static ssize_t char_dev_write(struct file *file, const char __user *buf, size_t len, loff_t *loff_t)
+{
+    int rst, i;
+    char write_buf[5] = {0};
+		struct char_dev *dev = file->private_data;
+    len = len > 5 ? 5:len;
+
+    printk("char dev write\n");
+		printk("file private data---device name:%s\n", dev_name(dev->device));
+
+    rst = copy_from_user(write_buf, buf, len);
+    if(0 != rst) {
+      return -1;
+    }
+
+    for(i=0; i < len; i++) {
+      printk("write data[%d] = %x\n", i, write_buf[i]);
+    }
+
+    return 0;
+}
+
+static int char_dev_release(struct inode *inode, struct file *file)
+{
+    printk("char dev release\n");
+
+    return 0;
+}
+
+static struct file_operations char_dev_fops = {
+    .owner   = THIS_MODULE,
+    .open    = char_dev_open,
+    .write   = char_dev_write,
+    .release = char_dev_release,
+};
+
+/* 模块加载时会调用的函数 */
+static int __init char_dev_init(void)
+{
+	/* 注册设备号 */
+	alloc_chrdev_region(&char_dev.devid, DEV_MINOR, DEVID_COUNT, DEVICE_NAME);
+
+	/* 初始化字符设备结构体 */
+	cdev_init(&char_dev.cdev, &char_dev_fops);
+
+	/* 注册字符设备 */
+	cdev_add(&char_dev.cdev, char_dev.devid, DRIVE_COUNT);
+
+	/* 创建类 */
+	char_dev.class = class_create(THIS_MODULE, DEVICE_NAME);
+	if(IS_ERR(char_dev.class))
+	{
+		return PTR_ERR(char_dev.class);
+	}
+
+	/* 创建设备节点 */
+	char_dev.device = device_create(char_dev.class, NULL, char_dev.devid, &char_dev, DEVICE_NAME);
+	if (IS_ERR(char_dev.device))
+	{
+		return PTR_ERR(char_dev.device);
+	}
+
+  printk("char dev init ok\n");
+
+  return 0;
+}
+
+/* 卸载模块 */
+static void __exit char_dev_exit(void)
+{
+	/* 注销字符设备 */
+	cdev_del(&char_dev.cdev);
+
+	/* 注销设备号 */
+	unregister_chrdev_region(char_dev.devid, DEVID_COUNT);
+
+	/* 删除设备节点 */
+	device_destroy(char_dev.class, char_dev.devid);
+
+	/* 删除类 */
+	class_destroy(char_dev.class);
+
+  printk("char dev exit ok\n");
+}
+
+/* 标记加载、卸载函数 */
+module_init(char_dev_init);
+module_exit(char_dev_exit);
+
+/* 驱动描述信息 */
+MODULE_AUTHOR("xiaoma <machengyuan@coinv.com>");
+MODULE_DESCRIPTION("char device driver");
+MODULE_VERSION("v1.0");
+MODULE_LICENSE("GPL");
---
+++ 1.字符设备/test/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+    int fd,i;
+    char buf[10];
+
+    fd = open(argv[1], O_RDWR);
+    if(fd < 0)
+    {
+        printf("Can't open file %s\r\n", argv[1]);
+        return -1;
+    }
+
+    for(i = 0;i < 10;i++)
+      buf[i] = i + 1;
+    write(fd, buf, 10);
+
+    close(fd);
+    return 0;
+}
