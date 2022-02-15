c422ba032e0618e63947563880b3917e0ecfdfc8
machengyuan <machengyuan@coinv.com>
2022-02-15 01:46:01 -0800

    增加错误退出路径

--- 1.字符设备/driver/c-drv.c
+++ 1.字符设备/driver/c-drv.c
@@ -34,82 +34,99 @@ static struct char_dev char_dev = {
 
 static int char_dev_open(struct inode *inode, struct file *file)
 {
-		struct char_dev *dev = container_of(inode->i_cdev, struct char_dev, cdev);
-		struct char_dev *drvdata = dev_get_drvdata(dev->device);
+	struct char_dev *dev = container_of(inode->i_cdev, struct char_dev, cdev);
+	struct char_dev *drvdata = dev_get_drvdata(dev->device);
 
-		printk("device name:%s\n", dev_name(dev->device));
-		printk("driver data device name:%s\n", dev_name(drvdata->device));
-    printk("char dev open\n");
+	printk("device name:%s\n", dev_name(dev->device));
+	printk("driver data device name:%s\n", dev_name(drvdata->device));
+	printk("char dev open\n");
 
-		file->private_data = dev;
-    return 0;
+	file->private_data = dev;
+	return 0;
 }
 
 static ssize_t char_dev_write(struct file *file, const char __user *buf, size_t len, loff_t *loff_t)
 {
-    int rst, i;
-    char write_buf[5] = {0};
-		struct char_dev *dev = file->private_data;
-    len = len > 5 ? 5:len;
+	int rst, i;
+	char write_buf[5] = {0};
+	struct char_dev *dev = file->private_data;
+	len = len > 5 ? 5:len;
 
-    printk("char dev write\n");
-		printk("file private data---device name:%s\n", dev_name(dev->device));
+	printk("char dev write\n");
+	printk("file private data---device name:%s\n", dev_name(dev->device));
 
-    rst = copy_from_user(write_buf, buf, len);
-    if(0 != rst) {
-      return -1;
-    }
+	rst = copy_from_user(write_buf, buf, len);
+	if(0 != rst) {
+	  return -1;
+	}
 
-    for(i=0; i < len; i++) {
-      printk("write data[%d] = %x\n", i, write_buf[i]);
-    }
+	for(i=0; i < len; i++) {
+	  printk("write data[%d] = %x\n", i, write_buf[i]);
+	}
 
-    return 0;
+	return len;
 }
 
 static int char_dev_release(struct inode *inode, struct file *file)
 {
-    printk("char dev release\n");
+	printk("char dev release\n");
 
-    return 0;
+	return 0;
 }
 
 static struct file_operations char_dev_fops = {
-    .owner   = THIS_MODULE,
-    .open    = char_dev_open,
-    .write   = char_dev_write,
-    .release = char_dev_release,
+	.owner   = THIS_MODULE,
+	.open    = char_dev_open,
+	.write   = char_dev_write,
+	.release = char_dev_release,
 };
 
 /* 模块加载时会调用的函数 */
 static int __init char_dev_init(void)
 {
+	int ret;
+
 	/* 注册设备号 */
-	alloc_chrdev_region(&char_dev.devid, DEV_MINOR, DEVID_COUNT, DEVICE_NAME);
+	if (alloc_chrdev_region(&char_dev.devid, DEV_MINOR, DEVID_COUNT, DEVICE_NAME)) {
+		printk("unable to allocate chrdev region\n");
+    return -EFAULT;
+	}
 
 	/* 初始化字符设备结构体 */
 	cdev_init(&char_dev.cdev, &char_dev_fops);
 
 	/* 注册字符设备 */
-	cdev_add(&char_dev.cdev, char_dev.devid, DRIVE_COUNT);
+	if (cdev_add(&char_dev.cdev, char_dev.devid, DRIVE_COUNT)) {
+		ret = -EFAULT;
+    goto err_chrdev_unreg;
+	}
 
 	/* 创建类 */
 	char_dev.class = class_create(THIS_MODULE, DEVICE_NAME);
 	if(IS_ERR(char_dev.class))
 	{
-		return PTR_ERR(char_dev.class);
+		ret = PTR_ERR(char_dev.class);
+    goto err_cdev_del;
 	}
 
 	/* 创建设备节点 */
 	char_dev.device = device_create(char_dev.class, NULL, char_dev.devid, &char_dev, DEVICE_NAME);
 	if (IS_ERR(char_dev.device))
 	{
-		return PTR_ERR(char_dev.device);
+		ret = PTR_ERR(char_dev.device);
+		goto err_class_destr;
 	}
 
   printk("char dev init ok\n");
 
   return 0;
+err_class_destr:
+  class_destroy(char_dev.class);
+err_cdev_del:
+  cdev_del(&char_dev.cdev);
+err_chrdev_unreg:
+  unregister_chrdev_region(char_dev.devid, DEVID_COUNT);
+	return ret;
 }
 
 /* 卸载模块 */
