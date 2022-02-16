c281f44128284efd9b8d83c94b7639387f08c797
machengyuan <machengyuan@coinv.com>
2022-02-15 23:41:37 -0800

    format code

--- 1.字符设备/test/main.c
+++ 1.字符设备/test/main.c
@@ -5,18 +5,17 @@
 
 int main(int argc, char **argv)
 {
-    int fd,i;
+    int fd, i;
     char buf[10];
 
     fd = open(argv[1], O_RDWR);
-    if(fd < 0)
-    {
+    if (fd < 0) {
         printf("Can't open file %s\r\n", argv[1]);
         return -1;
     }
 
-    for(i = 0;i < 10;i++)
-      buf[i] = i + 1;
+    for (i = 0; i < 10; i++)
+        buf[i] = i + 1;
     write(fd, buf, 10);
 
     close(fd);
