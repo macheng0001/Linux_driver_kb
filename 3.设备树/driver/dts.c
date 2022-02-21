91fc939c3a167ceb777f7dc494fc02b71803a853
machengyuan <machengyuan@coinv.com>
2022-02-20 21:35:38 -0800

    format code

--- 3.设备树/driver/dts.c
+++ 3.设备树/driver/dts.c
@@ -5,58 +5,55 @@
 
 static int dts_test_init(void)
 {
-  int ret;
-  unsigned int array[4], data, i;
-  const char *str;
-  struct device_node *np;
+    int ret;
+    unsigned int array[4], data, i;
+    const char *str;
+    struct device_node *np;
 
-  printk("dts test init\n");
-  np = of_find_node_by_name(NULL, "dts_test");
-  if(!np) {
-    printk("failed to find dts_test node\n");
-    return -ENODEV;
-  }
-
-  ret = of_property_read_u32_array(np, "dts-test-u32-array", array, 4);
-  if(ret) {
-    printk("failed to read array property:%d\n", ret);
-    return ret;
-  } else {
-    for(i=0; i < 4; i++) {
-      printk("dts-test-u32-array[%d] = %x\n", i, array[i]);
+    printk("dts test init\n");
+    np = of_find_node_by_name(NULL, "dts_test");
+    if (!np) {
+        printk("failed to find dts_test node\n");
+        return -ENODEV;
     }
-  }
 
-  ret = of_property_read_u32(np, "dts-test-u32", &data);
-  if(ret) {
-    printk("failed to read u32 property:%d\n", ret);
-    return ret;
-  } else {
-    printk("dts-test-u32:%x\n", data);
-  }
+    ret = of_property_read_u32_array(np, "dts-test-u32-array", array, 4);
+    if (ret) {
+        printk("failed to read array property:%d\n", ret);
+        return ret;
+    } else {
+        for (i = 0; i < 4; i++) {
+            printk("dts-test-u32-array[%d] = %x\n", i, array[i]);
+        }
+    }
 
-  ret = of_property_read_string(np, "dts-test-string", &str);
-  if(ret) {
-    printk("failed to read string property:%d\n", ret);
-    return ret;
-  } else {
-    printk("dts-test-string:%s\n", str);
-  }
+    ret = of_property_read_u32(np, "dts-test-u32", &data);
+    if (ret) {
+        printk("failed to read u32 property:%d\n", ret);
+        return ret;
+    } else {
+        printk("dts-test-u32:%x\n", data);
+    }
 
-  if (of_property_read_bool(np, "dts-test-bool")) {
-    printk("succeeded to read bool property\n");
-  } else {
-    printk("failed to read bool property\n");
-    return -EINVAL;
-  }
+    ret = of_property_read_string(np, "dts-test-string", &str);
+    if (ret) {
+        printk("failed to read string property:%d\n", ret);
+        return ret;
+    } else {
+        printk("dts-test-string:%s\n", str);
+    }
 
-  return  0;
+    if (of_property_read_bool(np, "dts-test-bool")) {
+        printk("succeeded to read bool property\n");
+    } else {
+        printk("failed to read bool property\n");
+        return -EINVAL;
+    }
+
+    return 0;
 }
 
-static void dts_test_exit(void)
-{
-  printk( "dts test exit\n");
-}
+static void dts_test_exit(void) { printk("dts test exit\n"); }
 
 module_init(dts_test_init);
 module_exit(dts_test_exit);
